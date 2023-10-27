16a320a352c3f9f46d1c3e3b40e421c49d451ed3
Sebastian <seb154@live.com>
2023-10-26 22:58:16 -0400

    modifies index.js and package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(arrayOfGames) {
+    const winningGame = arrayOfGames.find(game => game.result === "W");
+    return winningGame ? winningGame.year : undefined;
+  }
